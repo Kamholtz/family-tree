e3650520daedc9ab97271ebb4d3529da7ad2fb36
Carl <Carl.Kamholtz@gmail.com>
2020-09-12 12:07:59 +1000

    Reading a file

--- loadtree.js
+++ loadtree.js
@@ -48,7 +48,21 @@ treeData = [{
 	}]
 }]
 
+function readTextFile(file) {
+	var rawFile = new XMLHttpRequest();
+	rawFile.open("GET", file, false);
+	rawFile.onreadystatechange = function () {
+		if (rawFile.readyState === 4) {
+			if (rawFile.status === 200 || rawFile.status == 0) {
+				var allText = rawFile.responseText;
+				alert(allText);
+			}
+		}
+	}
+	rawFile.send(null);
+}
 
+readTextFile("family_tree.txt");
 
 dTree.init(treeData, {
 	target: "#graph",
